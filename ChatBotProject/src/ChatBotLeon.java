b3a7152591958fb416d23055bfad9012b19341cc
Leon <Leon@Loen>
2017-10-12 01:26:14 -0400

    Final

--- ChatBotProject/src/ChatBotLeon.java
+++ ChatBotProject/src/ChatBotLeon.java
@@ -1,4 +1,5 @@
  import java.util.Random;
+import java.util.Scanner;
  /*
   * Leon You 
   * Mr. Levin 
@@ -14,22 +15,42 @@ public class ChatBotLeon {
 	{
 		String response = "";
 		
+		if(findKeyword(statement, "no") >=0)
+		{
+			System.out.println("No? Well back to the main page you go!");
+			ChatBotRunner chatbot1 = new ChatBotRunner();
+			String[] args = new String[0] ;
+		    chatbot1.main(args);
+		}
 		if (statement.length() == 0)
 		{
 			response = "Say something, please.";
 		}
-		
+		if(findKeyword(statement, "fact") >=0)
+		{
+			System.out.println("Have a fact!");
+			response = getRandomFact();
+		}
 		else if (findKeyword(statement, "skins") >= 0)
 		{
-			response = topicSkins(statement);
+			System.out.println ("My favorite skin is the AWP Dragonlore, How much have you spent on skins?");		
+			Scanner a = new Scanner (System.in);
+			String skinStatement = a.nextLine();
+			response = topicSkins(skinStatement);
 		}
 		else if (findKeyword(statement, "weapons") >= 0)
 		{
-			response = topicWeapons(statement);
+			System.out.println ("My favorite rifle is the AK-47, what's yours?");
+			Scanner a = new Scanner (System.in);
+			String weaponStatement = a.nextLine();
+			response = topicWeapons(weaponStatement);
 		}
 		else if (findKeyword(statement, "maps") >= 0)
 		{
-			response = topicMaps(statement);
+			System.out.println ("My favorite map is Dust 2, what's yours?");
+			Scanner a = new Scanner (System.in);
+			String mapStatement = a.nextLine();
+			response = topicMaps(mapStatement);
 		}
 		else 
 		{
@@ -39,24 +60,101 @@ public class ChatBotLeon {
 	}
 	private String topicSkins(String statement)
 	{
-		return "My favorite skin is the AWP Dragonlore, what's yours?";		
-		
-		if (findKeyword(statement, "skins") >= 0)
+		int money = Integer.parseInt(statement);
+		if(money<=100)
 		{
-			response = topicSkins2(statement);
+			return "Wow, you've spent less than me!";
 		}
-	}
-	private String topicSkins2(String statement)
-	{
-		
+		else
+		{
+			return "Wow, you've spent more than me!";
+		}
 	}
 	private String topicWeapons(String statement)
 	{
-		return "My favorite weapon is the AK-47, what's yours?";
+		if(findKeyword(statement, "M4A1")>=0)
+		{
+			return "M4A1 is a pretty accurate rifle!";
+		}
+		else if(findKeyword(statement, "Galil Ar")>=0)
+		{
+			return "That's a pretty rare rifle to like!";
+		}
+		else if(findKeyword(statement, "M4A4")>=0)
+		{
+			return "M4A4's a pretty accurate rifle!!";
+		}
+		else if(findKeyword(statement, "AUG")>=0)
+		{
+			return "AUG's has a nice scope!";
+		}
+		else if(findKeyword(statement, "Scout")>=0)
+		{
+			return "The Scout has nice jump shot accuracy!";
+		}
+		else if(findKeyword(statement, "Famas")>=0)
+		{
+			return "Famas is a good budget rifle!";
+		}
+		else if(findKeyword(statement, "SG")>=0)
+		{
+			return "The SG has a pretty good scope!";
+		}
+		else if(findKeyword(statement, "AWP")>=0)
+		{
+			return "Best gun in the game Amirite!!";
+		}
+		else if(findKeyword(statement, "Auto")>=0)
+		{
+			return "I can't be friends with you I'm sorry!";
+		}
+		else if(findKeyword(statement, "AK47")>=0)
+		{
+			return "Ayyy, let's go!";
+		}
+		else
+		{
+			return "Sorry, I Haven't heard of that rifle.";
+		}
 	}
 	private String topicMaps(String statement)
 	{
-		return "My favorite map is Dust 2, what's yours?";
+		if(findKeyword(statement, "inferno")>=0)
+		{
+			return "Inferno's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "overpass")>=0)
+		{
+			return "Overpass' a pretty cool map!";
+		}
+		else if(findKeyword(statement, "nuke")>=0)
+		{
+			return "Nuke's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "cobblestone")>=0)
+		{
+			return "Cobblestone's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "train")>=0)
+		{
+			return "Train's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "mirage")>=0)
+		{
+			return "Mirage's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "cache")>=0)
+		{
+			return "Cache's a pretty cool map!";
+		}
+		else if(findKeyword(statement, "Dust 2")>=0)
+		{
+			return "Thats your favorite map too? That's awesome!!";
+		}
+		else
+		{
+			return "Sorry, I Haven't heard of that map.";
+		}
 	}
 	// took from ChatBotLevin
 	private int findKeyword(String statement, String goal, int startPos)
@@ -110,6 +208,15 @@ public class ChatBotLeon {
 	{
 		return findKeyword (statement, goal, 0);
 	}
-	private String [] listSkins = {};
-	private String [] listGuns = {"M4A1", "AWP", "AUG", "galil", "FAMAS"};
+	private String getRandomFact()
+	{
+		Random r = new Random ();
+		return randomFact [r.nextInt(randomFact.length)];
+	}
+	private String [] randomFact = 
+	{
+			"When it first launched, CS:GO didn’t have Competitive Mode and Skins.",
+			"You can actually pull out money from ATM beside bank at Overpass.",
+			"Chickens follow you if you press \"E\""
+	};
 }
